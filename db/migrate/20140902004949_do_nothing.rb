9bb6a054fd37a4e7d394d41d4f5732ec595b1435
Brendan S <sheehybrendan@aol.com>
2014-09-01 22:49:44 -0400

    migrations

---
+++ db/migrate/20140902004949_do_nothing.rb
@@ -0,0 +1,9 @@
+class DoNothing < ActiveRecord::Migration
+	
+  def up
+  end
+
+  def down
+  end
+
+end
---
+++ db/migrate/20140902005711_create_trainers.rb
@@ -0,0 +1,20 @@
+class CreateTrainers < ActiveRecord::Migration
+
+  def up
+    create_table :trainers do |t|
+    	# t.column "name", :string
+    	t.string "name"
+    	t.string "email", :default => "", :null => false
+
+    	# t.datetime "created_at"
+    	# t.datetime "updated_at"
+
+      t.timestamps
+    end
+  end
+
+  def down
+  	drop_table :trainers
+  end
+
+end
